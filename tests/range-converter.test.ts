579eb6b7e5621ce94aae1ff1d7a87c9c4dc87de3
darkgraycat <jburyanov@gmail.com>
2021-09-30 09:07:23 +0300

    write some tests

---
+++ tests/range-converter.test.ts
@@ -0,0 +1,13 @@
+import { rangeConvert } from '../src/range-converter';
+
+test('Convert 1m to cm', async () => {
+  expect(await rangeConvert(
+    { distance: { unit: 'm', value: 1 }, convert_to: 'cm' },
+  )).toEqual({ unit: 'cm', value: 100 });
+});
+
+test('Conver 20ft to in', async () => {
+  expect(await rangeConvert(
+    { distance: { unit: 'ft', value: 20 }, convert_to: 'in' },
+  )).toEqual({ unit: 'in', value: 240 });
+});
